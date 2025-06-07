032c3180f26b0a4456c38e365de0ad3fae9d1ad1
joseretamero <retamerojtona@gmail.com>
2025-06-07 17:09:38 +0200

    ECLIPSE DEFINITIVO CON 2 TEST

--- uah_photsat_freertos/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ uah_photsat_freertos/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -3,18 +3,23 @@
 #include <public/emu_gss_v1.h>
 
 //TODO 10 Complete FT_UAH_PHOTSAT_SERV_129_0010  done
-#define FT_UAH_PHOTSAT_SERV_129_0010
+//#define FT_UAH_PHOTSAT_SERV_129_0010
 
-//#define FT_UAH_PHOTSAT_SERV_129_FDIR_0020
+#define FT_UAH_PHOTSAT_SERV_129_FDIR_0020
+
+#define FT_0010_TIME_step0 (OBT_AFTER_POWER_ON + 5)
+#define FT_0010_TIME_step1 (OBT_AFTER_POWER_ON + 8)
+#define FT_0010_TIME_step2 (OBT_AFTER_POWER_ON +100)
 
 
 #ifdef FT_UAH_PHOTSAT_SERV_129_0010
-EmuGSS_TCProgram129_2 prog_FT_0010_step0(OBT_AFTER_POWER_ON + 4,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0010, Set PID Kp=Ki=Kd=0.5 to force max values",0.07, 0.02, 0.01);
+EmuGSS_TCProgram129_2 prog_FT_0010_step0(FT_0010_TIME_step0,
+    "FT_UAH_PHOTSAT_FDIR_0010 step 0, Set PID Params to 0.9, 0.07, 0.04",0.9, 0.07, 0.04);
 
-EmuGSS_TCProgram129_1 prog_FT_0010_step1(OBT_AFTER_POWER_ON + 5,
-	"FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Values to Theta, and number of samples",900,600,6);
+EmuGSS_TCProgram129_1 prog_FT_0010_step1(FT_0010_TIME_step1,
+	"FT_UAH_PHOTSAT_FDIR_0010 step 1, Observation ThetaPitch = 500 mins, ThetaYaw = 400 mins, take 6 images",500,400,6);
 
+EmuGSS_TCProgram128_1 prog_FT_0010_TIME_step_2(FT_0010_TIME_step2, "END FUNCTIONAL TEST");
 #endif
 
 
@@ -22,26 +27,49 @@ EmuGSS_TCProgram129_1 prog_FT_0010_step1(OBT_AFTER_POWER_ON + 5,
 
 #ifdef FT_UAH_PHOTSAT_SERV_129_FDIR_0020
 
-EmuGSS_TCProgram12_5_Value_UINT8 prog_FT_0020_step0(OBT_AFTER_POWER_ON + 10,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Define PMONID 1 to ThetaPitch_W_ControlPID",1,13,5,1,0x3,0,0x4003);
+#define FT_0020_TIME_step0 (OBT_AFTER_POWER_ON + 5)
+#define FT_0020_TIME_step1 (OBT_AFTER_POWER_ON + 10)
+#define FT_0020_TIME_step2 (OBT_AFTER_POWER_ON + 15)
+#define FT_0020_TIME_step3 (OBT_AFTER_POWER_ON + 20)
+#define FT_0020_TIME_step4 (OBT_AFTER_POWER_ON + 25)
+#define FT_0020_TIME_step5 (OBT_AFTER_POWER_ON + 30)
+#define FT_0020_TIME_step6 (OBT_AFTER_POWER_ON + 35)
+#define FT_0020_TIME_step7 (OBT_AFTER_POWER_ON + 40)
+#define FT_0020_TIME_step8 (OBT_AFTER_POWER_ON + 60)
 
-EmuGSS_TCProgram12_1 prog_FT_0020_step1(OBT_AFTER_POWER_ON + 15,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Enable PMONID 1 to ThetaPitch_W_ControlPID",1);
+EmuGSS_TCProgram12_5_Value_UINT8 prog_FT_0020_step_0(FT_0020_TIME_step0,
+		"FT_UAH_PHOTSAT_Monitoring_0020 step 0, Config PMODID 0 for monitoring PID 13",0, 13, 1, 1, 0xFF, 0, 0x4003);
 
-EmuGSS_TCProgram129_2 prog_FT_0020_step2(OBT_AFTER_POWER_ON + 20,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Set PID Kp=Ki=Kd=0.5 to force max values",0.5f, 0.5f, 0.5f);
 
-EmuGSS_TCProgram5_5 prog_FT_0020_step3(OBT_AFTER_POWER_ON + 25,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Enable EVID 0x4003",0x4003);
+EmuGSS_TCProgram12_5_Value_UINT8 prog_FT_0020_step_1(FT_0020_TIME_step1,
+		"FT_UAH_PHOTSAT_Monitoring_0020 step 1, Config PMODID 1 for monitoring PID 14",1, 14, 1, 1, 0xFF,  0, 0x4003);
 
-EmuGSS_TCProgram19_1_Action_129_3 prog_FT_0020_step4(OBT_AFTER_POWER_ON + 30,
-	"FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Set Event Action",0x4003);
 
-EmuGSS_TCProgram20_3_uint8 prog_FT_0020_step5(OBT_AFTER_POWER_ON + 35,
-    "FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Change PID 13 parameter to !=0",13,29);
+EmuGSS_TCProgram12_1 prog_FT_0020_step2(FT_0020_TIME_step2,
+		"FT_UAH_PHOTSAT_Event_Action_0020 step 2, Enable PMONID 0 " , 0);
 
-EmuGSS_TCProgram19_4 prog_FT_0020_step6( OBT_AFTER_POWER_ON + 40,
-	"FT_UAH_PHOTSAT_SERV_129_FDIR_0020, Enable Event Action", 0x4003);
+
+EmuGSS_TCProgram12_1 prog_FT_0020_step3(FT_0020_TIME_step3,
+		"FT_UAH_PHOTSAT_Event_Action_0020 step 3, Enable PMONID 1 " , 1);
+
+
+EmuGSS_TCProgram19_1_Action_129_3 prog_FT_0020_step_4(FT_0020_TIME_step4,
+		"FT_UAH_PHOTSAT_Event_Action_0020 step 4, 0x4003 Action: Attitude Control Reset",0x4003);
+
+
+EmuGSS_TCProgram19_4 prog_FT_0020_step5( FT_0020_TIME_step5,"FT_UAH_PHOTSAT_SERV_19_0020 Step 5, Enable Event Action", 0x4003);
+
+
+
+EmuGSS_TCProgram129_2 prog_FT_0020_TIME_step_6(FT_0020_TIME_step6,
+"FT_UAH_PHOTSAT_SERV_129_0020 step 6, Set Kp = Ki = Kd = 0.5" ,0.5,0.5,0.5);
+
+EmuGSS_TCProgram129_1 prog_FT_0020_TIME_step_7(FT_0020_TIME_step7,
+"FT_UAH_PHOTSAT_SERV_129_0010 step 2, Set Theta Pitch, Set Theta Yaw and Number of Images"
+		,15*50,15*50,5);
+
+
+EmuGSS_TCProgram128_1 prog_FT_0020_TIME_step_8(FT_0020_TIME_step8, "END FUNCTIONAL TEST");
 
 
 
